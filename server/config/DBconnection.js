996d47bc57fabad79473850c5944a44fb5e6bb08
ANKUR SHARMA <ankur143513@gmail.com>
2025-12-13 00:02:49 +0530

    Fix formatting in DBconnection.js

--- server/config/DBconnection.js
+++ server/config/DBconnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+ 
 const DBconnection = async () => {
     try {
         await mongoose.connect(process.env.DB_URL);
